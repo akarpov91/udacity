040259eb7d5dd30c56ad8cfe073c9e373152a7ed
Alex Karpov <akarpov@groupon.com>
2017-11-09 10:02:04 -0600

    added comments to Movie init function

--- movie_website/media.py
+++ movie_website/media.py
@@ -12,7 +12,17 @@ class Movie():
     # This class provides a way to store movie related information
 
     def __init__(self, movie_title, movie_summary, movie_rating, poster_image_url, trailer_youtube_url, cast):
-        # initialize instance of class Movie
+        '''initializes instance of class Movie
+        Args:
+            movie_title: Holds the title of class Movie
+            movie_summary: Holds the summary of class Movie
+            movie_rating: Holds the rating of class Movie
+            poster_image_url: Holds the poster url of class Movie
+            trailer_youtube_url: Holds the trailer link of class Movie
+            cast: Holds the cast info of class Movie
+        Returns:
+            An instance of movie_title, movie_summary, movie_rating, poster_image_url, trailer_youtube_url, cast
+        '''
         self.movie_title = movie_title
         self.movie_summary = movie_summary
         self.movie_rating = movie_rating
